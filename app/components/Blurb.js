af766c6048d9fb3371b3249ba503619cf2d2fd0f
IbiminaFG <gogoibimina@gmail.com>
2024-02-23 11:04:47 +0100

    revert the blurb component

--- app/components/Blurb.js
+++ app/components/Blurb.js
@@ -1,29 +1,19 @@
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Blurb(props) {
-  return (
-    <div className="w-full">
-      <div className="w-full">
-        <Link href={props.link} className="w-full">
-          <Image
-            src={props.img}
-            width={500}
-            height={227}
-            alt="blurb"
-            className="w-full rounded-xl"
-          />
-        </Link>
-      </div>
-      <div>
-        <Link href={props.link}>
-          <h3 className="text-2xl font-semibold my-3 hover:underline">
-            {props.title}
-          </h3>
-        </Link>
-        <p>{props.text}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="self-stretch w-full md:w-1/3 md:justify-between lg:justify-between lg:w-1/4 my-6 mx-4">
+            <div>
+                <Link href={props.link}>
+                    <Image src={props.img} width={500} height={227} alt="blurb" />
+                </Link>
+            </div>
+            <div>
+                <Link href={props.link}><h3 className="text-2xl font-semibold my-3 hover:underline">{props.title}</h3></Link>
+                <p>{props.text}</p>
+            </div>
+        </div>
+    )
 }
