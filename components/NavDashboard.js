3a2159c7b33c558bb0f8176db2aaf5ff2af2ecee
IbiminaFG <gogoibimina@gmail.com>
2024-02-23 06:42:35 +0100

    made some adjustements to the dashboard and navigations

--- components/NavDashboard.js
+++ components/NavDashboard.js
@@ -1,15 +1,26 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBell} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function NavDashboard() {
-    return (
-        <nav className={'flex justify-end items-center'}>
-            <FontAwesomeIcon icon={faBell} size={'2x'} />
-            <Link href={'#'}>
-                <Image src={"/img/author.jpg"} alt={"username"} width={40} height={40} className={"rounded-3xl w-10 h-10 mx-6 hover:border hover:border-blue-600"} />
-            </Link>
-        </nav>
-    )
+  return (
+    <nav className={"flex gap-6 justify-end items-center"}>
+      <div className="relative">
+        <FontAwesomeIcon icon={faBell} size="lg" />
+        <div className="absolute -right-2 -top-2 w-4 text-xs h-4 rounded-full bg-red-500 flex justify-center items-center text-white">
+          3
+        </div>
+      </div>
+      <Link href={"#"}>
+        <Image
+          src={"/img/author.jpg"}
+          alt={"username"}
+          width={40}
+          height={40}
+          className={"rounded-3xl w-10 h-10 hover:border hover:border-blue-600"}
+        />
+      </Link>
+    </nav>
+  );
 }
