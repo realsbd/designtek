c1d5f74da8857976dad9578d3444e9012805a7e9
IbiminaFG <gogoibimina@gmail.com>
2024-03-19 20:00:23 +0100

    adjusted the pagination button

--- app/components/Pagination.js
+++ app/components/Pagination.js
@@ -6,35 +6,35 @@ export default function Pagination() {
     <div className="flex justify-end items-center gap-5 pr-4 py-3.5">
       <div className="rounded flex justify-between items-center gap-5">
         <button
-          className="text-white text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border bg-cyan-600 aspect-square h-6 px-2.5 border-solid border-cyan-600"
+          className="text-white text-justify text-base leading-6 tracking-tight whitespace-nowrap flex justify-center items-center rounded border bg-cyan-600 aspect-square h-6 px-2.5 border-solid border-cyan-600"
           aria-label="Button 1"
           role="button"
         >
           1
         </button>
         <button
-          className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
+          className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap flex justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
           aria-label="Button 2"
           role="button"
         >
           2
         </button>
         <button
-          className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
+          className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap flex justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
           aria-label="Button 3"
           role="button"
         >
           3
         </button>
         <button
-          className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
+          className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap flex justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
           aria-label="Button 4"
           role="button"
         >
           4
         </button>
         <button
-          className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
+          className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap flex justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
           aria-label="Button 5"
           role="button"
         >
