31be8bb2eabebc2882bffcc2069893191f1866a2
IbiminaFG <gogoibimina@gmail.com>
2024-03-19 19:45:00 +0100

    tidy up the about us

--- app/components/Pagination.js
+++ app/components/Pagination.js
@@ -1,47 +1,47 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faChevronRight} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-export default function Pagination () {
-    return (
-        <div className="mr-28 flex justify-end gap-5 pr-4 py-3.5">
-            <div className="items-stretch rounded flex justify-between gap-5">
-                <button
-                    className="text-white text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border bg-cyan-600 aspect-square h-6 px-2.5 border-solid border-cyan-600"
-                    aria-label="Button 1"
-                    role="button"
-                >
-                    1
-                </button>
-                <button
-                    className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
-                    aria-label="Button 2"
-                    role="button"
-                >
-                    2
-                </button>
-                <button
-                    className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
-                    aria-label="Button 3"
-                    role="button"
-                >
-                    3
-                </button>
-                <button
-                    className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
-                    aria-label="Button 4"
-                    role="button"
-                >
-                    4
-                </button>
-                <button
-                    className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
-                    aria-label="Button 5"
-                    role="button"
-                >
-                    5
-                </button>
-            </div>
-            <FontAwesomeIcon icon={faChevronRight} />
-        </div>
-    )
+export default function Pagination() {
+  return (
+    <div className="flex justify-end items-center gap-5 pr-4 py-3.5">
+      <div className="rounded flex justify-between items-center gap-5">
+        <button
+          className="text-white text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border bg-cyan-600 aspect-square h-6 px-2.5 border-solid border-cyan-600"
+          aria-label="Button 1"
+          role="button"
+        >
+          1
+        </button>
+        <button
+          className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
+          aria-label="Button 2"
+          role="button"
+        >
+          2
+        </button>
+        <button
+          className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
+          aria-label="Button 3"
+          role="button"
+        >
+          3
+        </button>
+        <button
+          className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
+          aria-label="Button 4"
+          role="button"
+        >
+          4
+        </button>
+        <button
+          className="text-black text-justify text-base leading-6 tracking-tight whitespace-nowrap justify-center items-center rounded border aspect-square h-6 px-2 border-solid border-cyan-600"
+          aria-label="Button 5"
+          role="button"
+        >
+          5
+        </button>
+      </div>
+      <FontAwesomeIcon icon={faChevronRight} />
+    </div>
+  );
 }
