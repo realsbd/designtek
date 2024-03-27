4c4ffdcba713b3967a9ed47416ce518b78838d2c
IbiminaFG <gogoibimina@gmail.com>
2024-03-27 12:59:38 +0100

    trying to fix build

--- middleware.js
+++ middleware.js
@@ -2,19 +2,19 @@ import { updateSession } from "./lib/auth";
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-//   //   update the session before checking if the user is authenticated
-//   const response = await updateSession(request);
-//   //     get URL path of the current request
-//   const requestPath = request.nextUrl.pathname;
-//   if (
-//     requestPath.startsWith("/dashboard") &&
-//     !response.cookies.get("session")
-//   ) {
-//     const url = request.nextUrl.clone();
-//     url.pathname = "/login";
-//     return NextResponse.redirect(url);
-//   }
-//   return response;
+  //   update the session before checking if the user is authenticated
+  const response = await updateSession(request);
+  //     get URL path of the current request
+  const requestPath = request.nextUrl.pathname;
+  if (
+    requestPath.startsWith("/dashboard") &&
+    !response.cookies.get("session")
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+  return response;
 }
 
 export const config = {
