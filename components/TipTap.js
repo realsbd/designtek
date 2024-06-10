55c8cd7d2dff8a792b8852a160a63bc1fdc9a59e
realsbd <abimbola.salawudeen@gmail.com>
2024-06-10 13:31:59 -0700

    increase height for editor

--- components/TipTap.js
+++ components/TipTap.js
@@ -15,7 +15,7 @@ const Tiptap = ({ onChange, content }) => {
     extensions: [StarterKit, Underline],
     editorProps: {
       attributes: {
-        class: "flex flex-col px-4 py-3 justify-start border-b border-r border-l border-gray-700 text-gray-400 items-start w-full gap-3 font-medium text-[16px] pt-4 rounded-bl-md rounded-br-md outline-none",
+        class: "flex flex-col px-4 py-3 justify-start border-b border-r border-l border-gray-700 text-gray-400 items-start w-full min-h-[400px] gap-3 font-medium text-[16px] pt-4 rounded-bl-md rounded-br-md outline-none",
       },
     },
     onUpdate: ({ editor }) => {
