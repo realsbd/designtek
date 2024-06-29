0da22be95d300a74b5fc6277b38c4afcadac0dfb
realsbd <abimbola.salawudeen@gmail.com>
2024-06-29 04:42:25 -0700

    worked on post create api

--- app/dashboard/add-post/page.js
+++ app/dashboard/add-post/page.js
@@ -1,12 +1,12 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import DashboardLayout from "@/app/components/Layout/DashboardLayout";
 import Tiptap from "@/components/TipTap";
 import { useUser } from "@/app/hooks/useUser";
 import { useRouter } from "next/navigation";
-import { getCategories } from "@/lib/posts";
+import { createPost, getCategories } from "@/lib/posts";
 import {
   Select,
   SelectContent,
@@ -25,9 +25,33 @@ export default function NewPost() {
   const [content, setContent] = useState()
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('');
+  const titleRef = useRef(null);
+  const [featureImg, setFeatureImg] = useState();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      // check if title is not empty
+      const title = titleRef.current.value;
+      if (!title || !content ||!selectedCategory ||!featureImg) {
+        throw new Error('Please fill all the fields');
+      }
+      // set all data to object
+      const data = {
+        title,
+        content,
+        category: selectedCategory,
+        featureImg,
+      }
+
+      // post data to api
+      const response = await createPost(data);
+      console.log("response:", response);
+      // console.log("title:", title, "content:", content, "category:", selectedCategory, "featureImg:", featureImg);
+    } catch (error) {
+      console.error('Error submitting post:', error);
+
+    }
 
   }
 
@@ -56,7 +80,7 @@ export default function NewPost() {
         console.error('Error fetching categories:', error);
       }
     }
-  
+
     fetchCategories();
 
   }, [user, router]);
@@ -103,27 +127,21 @@ export default function NewPost() {
           Add a New Post
         </div>
 
-        <input name="title" type="text" placeholder="Title" className="w-full px-4 py-2 rounded-sm border-2 border-gray-700 mb-5" />
+        <label>
+          <span>Upload a file</span>
+          <input type="file" name="featureImg" accept="image/*" onChange={(e) => setFeatureImg(e.target.files[0])} />
+        </label>
+        <input name="title" type="text" placeholder="Title" ref={titleRef} className="w-full px-4 py-2 rounded-sm border-2 border-gray-700 mb-5" />
         {/* select categories here  */}
-        <Select>
-          <SelectTrigger
-            className="w-[180px]"
-            value={selectedCategory}
-            onChange={handleCategoryChange}
-          >
-            <SelectValue placeholder="Select a Category" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Categories</SelectLabel>
-              {Array.isArray(categories) && categories.map((category) => (
-                <SelectItem key={category._id} value={category._id}>
-                  {category.title}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+        <label>
+          Select a Category:
+          <select name="selectedFruit" onChange={handleCategoryChange}>
+            {Array.isArray(categories) && categories.map((category) => (
+              <option key={category._id} value={category.title}>{category.title}</option>
+            ))}
+
+          </select>
+        </label>
         <Tiptap content={content}
           onChange={(newContent) => handleContentChange(newContent)}
         />
